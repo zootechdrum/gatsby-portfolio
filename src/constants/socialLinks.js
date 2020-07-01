4de408b5a59d0831a5fc6c74e234ae82f02dee6c
Cesar Gomez <zootechdrum@Cesars-MacBook-Pro.local>
2020-06-30 20:41:14 -0700

    changed links to social media sites

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,37 +1,21 @@
 import React from "react"
-import {
-  FaFacebookSquare,
-  FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
-  FaTwitterSquare,
-} from "react-icons/fa"
+import { FaFacebookSquare, FaLinkedin, GoMarkGithub } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/profile.php?id=100003995021026",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/cesar-gomez-zootechnic/",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    icon: <GoMarkGithub className="social-icon"></GoMarkGithub>,
+    url: "https://github.com/zootechdrum",
   },
 ]
 const links = data.map(link => {
