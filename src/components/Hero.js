9ac29f0c7413b7b5fc4519cdf4f11f073790175b
Cesar Gomez <zootechdrum@gmail.com>
2020-07-03 16:31:37 -0700

    changed hero image and succ called image api using graphql

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,33 @@ import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
-
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article classNam="hero-info"></article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+      hero component
+    </header>
+  )
 }
 
 export default Hero
