8010690a5f9cba3cd0bd635144cb1e54b3b8f713
Cesar Gomez <zootechdrum@gmail.com>
2020-10-03 22:44:41 -0700

    creating the job component field

---
+++ portfolio-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd34f8df9-716c-4d48-87c5-663e04cec760'
+};
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,7 +4,30 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { order: DESC, fields: strapiId }) {
+      nodes {
+        company
+        description {
+          id
+          name
+        }
+        date
+        position
+      }
+    }
+  }
+`
+
 const Jobs = () => {
+  const data = useStaticQuery(query)
+
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, description } = jobs[0]
   return <h2>jobs component</h2>
 }
 
