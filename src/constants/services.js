d4ef02ab83b4d8426222d8a258a5442851ac527d
Cesar Gomez <zootechdrum@gmail.com>
2020-10-17 21:01:07 -0700

    replaced lorem ipsum with content from person experiance

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,22 @@
 import React from "react"
-import { FaCode, FaBook, FaAndroid } from "react-icons/fa"
+import { FaCode, FaBook, FaBookReader} from "react-icons/fa"
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
+    title: "Web Development Instructor",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 2,
     icon: <FaBook className="service-icon" />,
-    title: "Web development Tutor",
+    title: "Web Development Tutor",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "Web development Instructor",
+    icon: <FaBookReader className="service-icon" />,
+    title: "Web Development T.A ",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
 ]
