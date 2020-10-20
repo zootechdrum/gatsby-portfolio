8b33626b1698137e1e67c849435168c5b7981b4f
Cesar Gomez <zootechdrum@gmail.com>
2020-10-19 22:06:19 -0700

    added first project to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+const Project = ({ description, title, github, stack, url, image, index }) => {
+  return (
+    <article className="project">
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      <div className="project-info">
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default ({data}) => {
       <Hero />
       <Services />
       <Jobs />
-      <Projects projects={projects} title="featured projects" />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
