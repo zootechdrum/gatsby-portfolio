afff8cea98653f8b054ae43ea56dc6a220381e7b
Cesar Gomez <zootechdrum@gmail.com>
2020-10-25 19:49:48 -0700

    added new resume and added menu for mobile devices

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,29 @@
 import React from "react"
-import { FaCode, FaBook, FaBookReader} from "react-icons/fa"
+import { FaCode, FaBook, FaBookReader } from "react-icons/fa"
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "Web Development Instructor",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `At the end of the week (Saturdays), I spent 4 hours delivering
+    in-person workshops covering topics such as version control
+    with Git and GitHub, HTML, CSS, JavaScript, Bootstrap, React, NodeJS, and MongoDB.Graded the student's weekly web development assignments `,
   },
   {
     id: 2,
     icon: <FaBook className="service-icon" />,
     title: "Web Development Tutor",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Lead one-on-one sessions with students over a one hour long
+    video conference each week discussing popular topics such as
+    Node, JQuery, MySql, Mongo, BootStrap, CSS3, React, and
+    HTML5. `,
   },
   {
     id: 3,
     icon: <FaBookReader className="service-icon" />,
     title: "Web Development T.A ",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Assigned grades for coursework that used popular web
+    development technologies such as JavaScript, Bootstrap, Node, CSS, Mongo,HTML5 and MySQL, and React. Collaborated with instructional team members to best meet
+    student needs. `,
   },
 ]
